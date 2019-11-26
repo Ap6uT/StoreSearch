3e9ec39d1a0c3533a53dda66dd711a769b132f05
Ap6uT <2Ap6uT@gmail.com>
2019-11-26 22:53:59 +0300

    Chapter 33: Custom Table Cells

---
+++ StoreSearch/StoreSearch/SearchResultCell.swift
@@ -0,0 +1,31 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Alexander Grishin on 26.11.2019.
+//  Copyright © 2019 Alexander Grishin. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        selectedBackgroundView = selectedView
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
