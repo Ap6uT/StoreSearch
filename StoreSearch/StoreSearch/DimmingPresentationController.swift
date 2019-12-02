3f1d98fffce4e17b58402d2c8ab49438a529f1b3
Ap6uT <2Ap6uT@gmail.com>
2019-12-02 22:01:58 +0300

    Chapter 37: The Detail Pop-Up

---
+++ StoreSearch/StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,16 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Alexander Grishin on 02.12.2019.
+//  Copyright © 2019 Alexander Grishin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+    override var shouldRemovePresentersView: Bool {
+        return false
+    }
+}
